4b96a991b6e3550dbd1c0eb3f6aac80bd7492fc3
Nathaniel Collins <nathancleon@gmail.com>
2015-10-19 21:56:27 -0500

    Changed resumeBuilder.js to .prepend() to display at top of header id

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,5 +5,5 @@ var name = "Nathaniel Collins";
 var role = "Front-End Web Developer"
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-$("#header").append(formattedRole);
