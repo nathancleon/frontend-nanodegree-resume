5c91e0b077b900d3e55e5b3ac723c1c23c77ca04
Nathaniel Collins <nathancleon@gmail.com>
2015-10-19 21:37:37 -0500

    Mixing .replace() and .append() Udacity exercise

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Nathaniel Collins";
+var role = "Front-End Web Developer"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
