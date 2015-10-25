ddb565f8949ecd3e863e40cbb0351d139646338f
Nathaniel Collins <nathancleon@gmail.com>
2015-10-25 14:34:22 -0500

    Added urls and added a job to the work JSON. Changed JSON order.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,7 +10,7 @@ var bio = {
     },
     "welcomeMessage": "I'm really excited to learn web development and I feel like I am finally making progress!",
     "skills": [
-        "HTML", "CSS", "JavaScript", "Insurance", "motivator"
+        "HTML", "CSS", "JavaScript", "insurance", "research", "Excel"
     ],
     "bioPic": "images/nathan.jpg"
 };
@@ -28,17 +28,33 @@ var education = {
     "onlineCourses": [
         {
             "title": "Front-End Web Development Nanodegree",
-            "school": "Udacity"
+            "school": "Udacity",
+            "url": "http://www.udacity.com"
         }
     ]
 };
 
-var project = {
+var work = [
+    {
+        "employer": "Texas Windstorm Insurance Association",
+        "title": "Quality Assurance Analyst",
+        "dates": "October 2015 - Present",
+        "Description": "Provided quality assurance for daily and catastrophe claim handling, prepared training documents and trained personal, prepared technical documents for management review and approval, prepared and presented to outside vendors, participated in management planning sessions. Completed quarterly audits of claim files and conducted slant audits to research trends in claim handling."
+    },
+    {
+        "employer": "Texas Windstorm Insurance Association",
+        "title": "Claims Examiner",
+        "dates": "September 2013 - October 2015",
+        "Description": "Tasks performed include adjusting reserves and providing reserve recommendations to ensure consistent reserving activities, conduct detailed bill reviews to implement claim management and expense control, contact and/or interview claimants to get additional information, examine claims investigated by insurance adjusters, further investigating questionable claims to determine payment authorization, present cases and participate in their discussion at claim committee meetings, resolve complex, severe exposure claims using high service oriented file handling, verify and analyze data used in settling claims to ensure claims are valid and that settlements are made according to company practices and procedures."
+    }
+];
+
+var projects = {
     "projects": [
         {
             "title": "About Me",
             "dates": "September, 2015",
-            "description": "Created an about me page using HTML and CSS."
+            "description": "Created an about me page using HTML and CSS as part of the Udacity Front-End Web Development Nanodegree."
         },
         {
             "title": "Portfolio",
@@ -46,11 +62,4 @@ var project = {
             "description": "Created portfolio page based on a mock up image provided by Udacity using Bootstrap."
         }
     ]
-};
-
-var work = {
-    "jobPosition": "Quality Assurance Analyst",
-    "employer": "Texas Windstorm Insurance Association",
-    "yearsWorked": "2",
-    "city": "Austin, Texas"
 };
