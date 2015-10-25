0b5e79175914249eda383b41b038b23238d810d7
Nathaniel Collins <nathancleon@gmail.com>
2015-10-25 14:01:14 -0500

    Updated resumeBuilder.js to add the 4 JSONS: bio, work, education, projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,57 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-var name = "Nathaniel Collins";
-var role = "Front-End Web Developer"
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var bio = {
+    "name": "Nathaniel Collins",
+    "role": "Front-End Web Developer",
+    "contacts": {
+        "mobile": "512-567-5674",
+        "email": "nathancleon@gmail.com",
+        "github": "nathancleon",
+        "twitter": "@nathancleon",
+        "location": "Austin, Texas"
+    },
+    "welcomeMessage": "I'm really excited to learn web development and I feel like I am finally making progress!",
+    "skills": [
+        "HTML", "CSS", "JavaScript", "Insurance", "motivator"
+    ],
+    "bioPic": "images/nathan.jpg"
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "St. Edward's University",
+            "city": "Austin, Texas",
+            "majors": "Psychology",
+            "graduationYear": "2013",
+            "url": "http://www.stedwards.edu"
+        },
+        {
+            "name": "Udacity",
+            "city": "Remote",
+            "majors": "Front-End Web Development",
+            "graduationYear": "pending",
+            "url": "http://www.udacity.com"
+        }
+    ]
+};
+
+var project = {
+    "projects": [
+        {
+            "title": "About Me",
+            "dates": "September, 2015",
+            "description": "Created an about me page using HTML and CSS."
+        },
+        {
+            "title": "Portfolio",
+            "dates": "October, 2015",
+            "description": "Created portfolio page based on a mock up image provided by Udacity using Bootstrap."
+        }
+    ]
+};
+
+var work = {
+    "jobPosition": "Quality Assurance Analyst",
+    "employer": "Texas Windstorm Insurance Association",
+    "yearsWorked": "2",
+    "city": "Austin, Texas"
+};
